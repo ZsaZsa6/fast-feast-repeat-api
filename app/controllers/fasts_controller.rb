965aaf7232a30f0d091bcca3f0864c7db641ca2f
Ronda Cowen <cowenhome5@gmail.com>
2021-02-02 11:45:41 -0600

    change to controller action from delete to destroy

--- app/controllers/fasts_controller.rb
+++ app/controllers/fasts_controller.rb
@@ -5,35 +5,29 @@ class FastsController < ApplicationController
         
         new_fast_hash = fasts.map do |fast|
             fast.calculate_hours.merge!(fast: fast)
-            # binding.pry
-           
         end
-
         render json: new_fast_hash
-        
-       
     end
 
     def show
         fast = Fast.find(params[:id])
         render json: fast.calculate_hours.merge!(fast: fast)
     end
+
     def create
         fast = Fast.create
         render json: fast.calculate_hours.merge!(fast: fast)
-       
     end
 
     def update
         fast = Fast.find_by(id: params[:id])
         fast.update(fast_params)
-        # binding.pry
         render json: fast.calculate_hours.merge!(fast: fast)
     end
 
-    def delete
+    def destroy
         fast = Fast.find(params[:id])
-        fast.destroy
+        fast.delete
     end
 
     private
@@ -42,5 +36,5 @@ class FastsController < ApplicationController
         params.permit(:id, :active)
     end
 end
-# time stamp.now strftime difference use  timedifference.between (created_at - Time.now).humanize
+
 
